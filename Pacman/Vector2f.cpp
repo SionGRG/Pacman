1f4510a150f875521a8acb37007f08ab54c44545
Sion GREG <45804912+SionGRG@users.noreply.github.com>
2023-02-12 15:54:08 +0000

    Pacman Update 1.2
    [Brief]
    -=-  Improving the Vector2f Class of Math Library -=-
    
    [project]
    
    [game]
    -  Move the class methods definitions to the cpp files and left the declarations in the header file
    - inspected the project to look for similar code structure that needed to change

--- Pacman/Vector2f.cpp
+++ Pacman/Vector2f.cpp
@@ -1,2 +1,85 @@
 #include "Vector2f.h"
 
+/*
+	Moved the class method definitions to the cpp files
+*/
+
+Vector2f::Vector2f()
+{
+	myX = 0.f;
+	myY = 0.f;
+}
+
+Vector2f::Vector2f(float anX, float anY)
+	: myX(anX), myY(anY)
+{
+}
+
+const Vector2f Vector2f::operator+(const Vector2f& other) const
+{
+	Vector2f v(myX + other.myX, myY + other.myY);
+	return v;
+}
+
+const Vector2f Vector2f::operator-(const Vector2f& other) const
+{
+	Vector2f v(myX - other.myX, myY - other.myY);
+	return v;
+}
+
+const Vector2f Vector2f::operator*(const Vector2f& other) const
+{
+	Vector2f v(myX * other.myX, myY * other.myY);
+	return v;
+}
+
+Vector2f& Vector2f::operator+=(const Vector2f& other)
+{
+	myX = myX + other.myX;
+	myY = myY + other.myY;
+
+	return *this;
+}
+
+Vector2f& Vector2f::operator-=(const Vector2f& other)
+{
+	myX = myX - other.myX;
+	myY = myY - other.myY;
+
+	return *this;
+}
+
+Vector2f& Vector2f::operator*=(const float aFloat)
+{
+	myX *= aFloat;
+	myY *= aFloat;
+
+	return *this;
+}
+
+Vector2f& Vector2f::operator/=(const float aFloat)
+{
+	myX /= aFloat;
+	myY /= aFloat;
+
+	return *this;
+}
+
+const Vector2f Vector2f::operator*(const float aValue) const
+{
+	Vector2f v(myX * aValue, myY * aValue);
+	return v;
+}
+
+float Vector2f::Length() const
+{
+	return sqrt(myX * myX + myY * myY);
+}
+
+void Vector2f::Normalize()
+{
+	float length = Length();
+
+	if (length > 0.f)
+		*this /= length;
+}
--- Pacman/Vector2f.h
+++ Pacman/Vector2f.h
@@ -3,82 +3,32 @@
 
 #include <math.h> 
 
+/*
+	Move the class methods definitions to the cpp files and left the declarations in the header file
+*/
+
 class Vector2f
 {
 public:
-	Vector2f()
-	{
-		myX = 0.f;
-		myY = 0.f;
-	}
+	Vector2f();
 
-	Vector2f(float anX, float anY)
-	{
-		myX = anX;
-		myY = anY;
-	}
+	Vector2f(float anX, float anY);
 
-	const Vector2f Vector2f::operator-(const Vector2f &other) const 
-	{
-		Vector2f v(myX - other.myX, myY - other.myY);
-		return v;
-	}
-
-	const Vector2f Vector2f::operator+(const Vector2f &other) const 
-	{
-		Vector2f v(myX + other.myX, myY + other.myY);
-		return v;
-	}
-
+	const Vector2f operator+(const Vector2f& other) const;
+	const Vector2f operator-(const Vector2f& other) const;
+	const Vector2f operator*(const Vector2f& other) const;
 	
-	const Vector2f Vector2f::operator*(const Vector2f& other) const 
-	{
-		Vector2f v(myX*other.myX, myY*other.myY);
-		return v;
-	}
+	Vector2f& operator+=(const Vector2f& other);
+	Vector2f& operator-=(const Vector2f& other);
+	Vector2f& operator*=(const float aFloat);
+	Vector2f& operator/=(const float aFloat);
 
-	Vector2f& Vector2f::operator+=(const Vector2f &other) 
-	{
-		myX = myX + other.myX;
-		myY = myY + other.myY;
+	// Scalar multiplication
+	const Vector2f operator*(const float aValue) const;
 
-		return *this;
-	}
+	float Length() const;
 
-	Vector2f& Vector2f::operator*=(const float aFloat) 
-	{
-		myX *= aFloat;
-		myY *= aFloat;
-
-		return *this;
-	}
-
-	Vector2f& Vector2f::operator/=(const float aFloat) 
-	{
-		myX /= aFloat;
-		myY /= aFloat;
-
-		return *this;
-	}
-
-	const Vector2f Vector2f::operator*(const float aValue) const 
-	{
-		Vector2f v(myX * aValue, myY * aValue);
-		return v;
-	}
-
-	float Vector2f::Length() const
-	{
-		return sqrt(myX*myX + myY*myY);
-	}
-
-	void Vector2f::Normalize()
-	{
-		float length = Length();
-
-		if (length > 0.f)
-			*this /= length;
-	}
+	void Normalize();
 
 	float myX;
 	float myY;
